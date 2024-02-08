f33a8b1802cd464b5db6e2b3fd9000dae6711419
Ronaldinho Ortega Ruiz <112998261+Ronaldinho-OrtegaRuiz@users.noreply.github.com>
2024-02-08 00:30:38 -0500

    Evento click e validación de texto

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
+const contentImg = document.getElementsByClassName('img-text');
+const resultContent = document.getElementsByClassName('result-content');
+const user_text = document.getElementById('user-text');
+const result_text = document.getElementById('result-text');
+const encrypt_btn = document.getElementById('encrypt-btn');
+const decrypt_btn = document.getElementById('decrypt-btn');
+const msg = document.getElementById('msg');
 
 
-function ocultarContenidoResultBar(){
-    const contentImg = document.getElementsByClassName('img-text');
-    const resultText = document.getElementsByClassName('result-text');
+encrypt_btn.addEventListener('click', function(){
+    let text = user_text.value.trim();
+    validarTexto(text);
+});
 
-    for (let i = 0; i < contentImg.length; i++) {
-        contentImg[i].style.display = 'none';
-    }
+decrypt_btn.addEventListener('click', function(){
+    let text = user_text.value.trim();
+    validarTexto(text);
+});
 
-    for (let i = 0; i < resultText.length; i++) {
-        resultText[i].style.display = 'flex';
+function validarTexto(text) {
+    if(text === '' || text === null || text === undefined) {
+        ocultarResultado();
+        msg.innerText = 'Ningún mensaje fue';
+    }else if(/^[a-z]*$/i.test(text)) {
+        mostrarResultado();
+    }else{
+        ocultarResultado();
+        msg.innerText = 'Un caracter no permitido fue';
     }
 }
 
+function mostrarResultado(){
+    resultContent[0].style.display = 'flex';
+    contentImg[0].style.display = 'none';
+    result_text.value = user_text.value;
+}
+
+function ocultarResultado(){
+    resultContent[0].style.display = 'none';
+    contentImg[0].style.display = 'flex';
+    msg.innerText = '';
+}
